755cfe25087d4bcf85bf870a36b49cb8548b7d64
KrayVlad <kray.vladyslav@gmail.com>
2022-06-27 13:55:15 +0300

    Moved from server directory

--- myserver.cpp
+++ myserver.cpp
@@ -1,2 +1,42 @@
+#include "myserver.h"
+#include "IDataBase.cpp"
+#include "IDataBase.h"
+#include "DBErrors.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+myserver::myserver(){}
+
+myserver::~myserver(){}
+
+void myserver::startServer(){
+    if(this->listen(QHostAddress::Any,5555)){
+        qDebug()<<"Listening";
+    }
+    else{
+        qDebug()<<"Not listening";
+    }
+}
+
 void myserver::incomingConnection(qintptr pisocketDescriptor){
+    socket = new QTcpSocket(this);
     socket->setSocketDescriptor(pisocketDescriptor);
+
+    connect(socket, SIGNAL(readyRead()),this,SLOT(sockReady()));
+    connect(socket, SIGNAL(disconnected()),this,SLOT(sockDisc()));
+
+    qDebug()<<pisocketDescriptor<<"Client connected";
+
+    socket->write("{\"type\":\"connect\",\"status\":\"yes\"}");
+    qDebug()<<"Send client connect status - YES";
+}
+
+void myserver::sockReady(){
+}
+
+
+void myserver::sockDisc(){
+    qDebug()<<"Disconnect";
+    socket->deleteLater();
+}
--- myserver.h
+++ myserver.h
@@ -1,1 +1,47 @@
+#ifndef MYSERVER_H
+#define MYSERVER_H
+
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonParseError>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QFile>
+#include <QString>
+#include <QByteArray>
+#include <QByteArrayView>
+#include <QtCore>
+#include <iostream>
+#include <QIODevice>
+#include <string>
+#include <vector>
+#include <memory.h>
+#include <QRegularExpression>
+#include <QStringConverter>
+
+class myserver: public QTcpServer{
+    Q_OBJECT
+public:
+    myserver();
+    ~myserver();
+
+    QTcpSocket* socket;
+    QByteArray Data;
+
+    QJsonDocument doc;
+    QJsonParseError docError;
+
+    QByteArray res;
+
+    QSqlDatabase db;
+
+public slots:
+    void startServer();
     void incomingConnection(qintptr pisocketDescriptor);
+    void sockReady();
+    void sockDisc();
+};
+
+#endif // MYSERVER_H
